ea415dd68a0c18241ab5ce4b5b4d9535f1594f0e
Dashiell Boyles <dashiellboyles@Dashiells-MacBook-Air.local>
2022-02-07 16:16:50 +0000

    Added extra routes to our server

--- app.rb
+++ app.rb
@@ -1,4 +1,13 @@
 require 'sinatra'
+require "sinatra/reloader" if development?
 get '/' do
     "hello!"
+end
+
+get '/secret' do
+  "I wish Ben would sort out his git pull's"
+end
+
+get '/names' do
+  "We are the coding massive"
 end
